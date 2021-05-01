3b6d0c96b39da84df7fb2ae2fab344f3bc4b8b0f
wender zacarias xavier <wender.xavier@thoughtworks.com>
2021-05-01 17:03:49 -0300

    add listeners

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import logo from "./logo.svg";
 import "./app.css";
 
@@ -6,6 +7,38 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App() {
+  miro.addListener("SELECTION_UPDATED", (widget) => {
+    console.log("SELECTION_UPDATED");
+    console.log(widget);
+  });
+
+  miro.addListener("WIDGETS_CREATED", (widget) => {
+    console.log("WIDGETS_CREATED");
+    console.log(widget);
+  });
+
+  miro.addListener("WIDGETS_DELETED", (widget) => {
+    console.log("WIDGETS_DELETED");
+    console.log(widget);
+  });
+
+  miro.addListener("WIDGETS_TRANSFORMATION_UPDATED", (widget) => {
+    console.log("WIDGETS_TRANSFORMATION_UPDATED");
+    console.log(widget);
+  });
+
+  miro.addListener("ALL_WIDGETS_LOADED", (widget) => {
+    console.log("ALL_WIDGETS_LOADED");
+    console.log(widget);
+  });
+
+  const handleClick = async () => {
+    let allStickers = await miro.board.widgets.get({ type: "shape" });
+    console.log(allStickers);
+    let allFrames = await miro.board.widgets.get({ type: "frame" });
+    console.log(allFrames);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,14 +46,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => handleClick()}>Click Here</button>
       </header>
     </div>
   );
