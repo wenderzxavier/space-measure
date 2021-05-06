4cc343647b5cf19ae0e7b95784d731f6e939fa29
wender zacarias xavier <wender.xavier@thoughtworks.com>
2021-05-06 14:24:40 -0300

    add listeners for WIDGETS_CREATED and WIDGETS_TRANSFORMATION_UPDATED on index page

--- src/App.js
+++ src/App.js
@@ -4,32 +4,15 @@ import "./app.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 
 function App() {
-  miro.addListener("SELECTION_UPDATED", (widget) => {
-    console.log("SELECTION_UPDATED");
-    console.log(widget);
-  });
-
-  miro.addListener("WIDGETS_CREATED", (widget) => {
-    console.log("WIDGETS_CREATED");
-    console.log(widget);
-  });
-
-  miro.addListener("WIDGETS_DELETED", (widget) => {
-    console.log("WIDGETS_DELETED");
-    console.log(widget);
-  });
-
-  miro.addListener("WIDGETS_TRANSFORMATION_UPDATED", (widget) => {
-    console.log("WIDGETS_TRANSFORMATION_UPDATED");
-    console.log(widget);
-  });
-
-  miro.addListener("ALL_WIDGETS_LOADED", (widget) => {
-    console.log("ALL_WIDGETS_LOADED");
-    console.log(widget);
+  miro.onReady(() => {
+    miro.addListener("SELECTION_UPDATED", (widget) => {
+      console.log("SELECTION_UPDATED");
+      console.log(widget);
+    });
   });
 
   const handleClick = async () => {
@@ -37,15 +20,15 @@ function App() {
     console.log(allStickers);
     let allFrames = await miro.board.widgets.get({ type: "frame" });
     console.log(allFrames);
+    let allCurves = await miro.board.widgets.get({ type: "curve" });
+    console.log(allCurves);
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>App version 1.12</p>
         <button onClick={() => handleClick()}>Click Here</button>
       </header>
     </div>
@@ -53,3 +36,24 @@ function App() {
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// 1) Scale input: The scale on architectural drawings are used to reflect a dimension on the drawing, which corresponds to a
+// dimension in real life. And example of a scale 1/8" = 1'; this means that 1/8 inches on the drawing will correspond to 1 feet in
+// real life. The dimensions could be in the metric system (m^2 and lm) or imperial system (Sqft and lf).
+
+// 2) Draw Polygon: I will need to draw polygons inside Miro boards. The number of pixels cover by the polygon, based on the input
+// scale, will result in the area of the polygon in SquareFeet or SquareMeters. I will be great if I will be able to draw rectangles
+// too, which a special type of polygons.
+
+// 3) Draw line: I will need to draw line segments inside Miro boards. The number of pixels cover by the line, based on the input
+// scale, will result in the length of the line in Feet or Meters.
+
+// 4) Count Shapes: I will need to count certain objects on architectural drawings. We can achieve that by clicking shapes on top
+// of the concerned object and the count those shapes. No scale or dimension is needed for the shapes... only their count.
+
+// 5) Report: I will need to be able to extract the information created using the web plug-in that will be built, into excel. The
+// idea is to compile all the information of every polygon, line, or count done on the Miro board and create a report that provides structured information.
+
+// All polygons, lines, and count will have specific properties that will be needed to be attached to each of them. Some of these properties
+// area: Name - Area - Type - Floor - Perimeter - Length - Count. As discussed on our call, these properties can be input using a new UI
+// inside Miro (example was the menu that appears on the left of the screen when selecting an object)
---
+++ src/calcs.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+const squareArea = (width, height) => width * height;
+
+const squarePerimeter = (width, height) => width * 2 + height * 2;
+
+export const updateShapeMetadata = async (widgetId) => {
+  const widget = await miro.board.widgets.get({ id: widgetId });
+  miro.board.widgets.update({
+    id: widget[0].id,
+    metadata: {
+      "3074457358398558776": {
+        area: squareArea(widget[0].width, widget[0].height),
+        perimeter: squarePerimeter(widget[0].width, widget[0].height),
+      },
+    },
+  });
+};
