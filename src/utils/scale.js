8dac03dac7e0d19b1567d34592c8abe18eeafa03
wender zacarias xavier <wender.xavier@thoughtworks.com>
2021-05-11 16:24:35 -0300

    add scale functions and constants

---
+++ src/utils/scale.js
@@ -0,0 +1,10 @@
+import { APP_ID } from "./constants";
+
+export const getCurrentScale = () => localStorage.getItem(`${APP_ID}_scale`);
+
+export const getScaleUnit = () => localStorage.getItem(`${APP_ID}_unit`);
+
+export const setScale = (scale) =>
+  localStorage.setItem(`${APP_ID}_scale`, scale);
+
+export const setUnit = (unit) => localStorage.setItem(`${APP_ID}_unit`, unit);
